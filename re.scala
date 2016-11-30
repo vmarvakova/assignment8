725f46fb2ab2e31f44e88c8c45fb97b8af52bda9
Viktoria Marvakova <viktoria.marvakova@kcl.ac.uk>
2016-11-30 14:27:47 +0000

    first commit

--- re.scala
+++ re.scala
@@ -5,15 +5,15 @@ abstract class Rexp
 case object ZERO extends Rexp
 case object ONE extends Rexp
 case class CHAR(c: Char) extends Rexp
-case class ALT(r1: Rexp, r2: Rexp) extends Rexp   // alternative 
+case class ALT(r1: Rexp, r2: Rexp) extends Rexp   // alternative
 case class SEQ(r1: Rexp, r2: Rexp) extends Rexp   // sequence
 case class STAR(r: Rexp) extends Rexp             // star
 
 
 // some convenience for typing in regular expressions
 
-import scala.language.implicitConversions    
-import scala.language.reflectiveCalls 
+import scala.language.implicitConversions
+import scala.language.reflectiveCalls
 
 def charlist2rexp(s: List[Char]): Rexp = s match {
   case Nil => ONE
@@ -36,59 +36,82 @@ implicit def stringOps (s: String) = new {
   def ~ (r: String) = SEQ(s, r)
 }
 
-// (1a) Complete the function nullable according to
-// the definition given in the coursework; this 
-// function checks whether a regular expression
-// can match the empty string
+def nullable (r: Rexp) : Boolean = r match {
+  case ZERO => false
+  case ONE => true
+  case CHAR(_) => false
+  case ALT(r1, r2) => nullable(r1) || nullable(r2)
+  case SEQ(r1, r2) => nullable(r1) && nullable(r2)
+  case STAR(_) => true
 
-def nullable (r: Rexp) : Boolean = ...
-
-
-// (1b) Complete the function der according to
-// the definition given in the coursework; this
-// function calculates the derivative of a 
-// regular expression w.r.t. a character
-
-def der (c: Char, r: Rexp) : Rexp = ...
-
-// (1c) Complete the function der according to
-// the specification given in the coursework; this
-// function simplifies a regular expression;
-// however it does not simplify inside STAR-regular
-// expressions
-
-def simp(r: Rexp) : Rexp = ... 
-
-// (1d) Complete the two functions below; the first 
-// calculates the derivative w.r.t. a string; the second
-// is the regular expression matcher taking a regular
-// expression and a string and checks whether the
-// string matches the regular expression
-
-def ders (s: List[Char], r: Rexp) : Rexp = ... 
-
-def matcher(r: Rexp, s: String): Boolean = ...
+}
 
 
-// (1e) Complete the function below: it searches (from the left to 
-// right) in string s1 all the non-empty substrings that match the 
-// regular expression -- these substrings are assumed to be
-// the longest substrings matched by the regular expression and
-// assumed to be non-overlapping. All these substrings in s1 are replaced
-// by s2.
+def der (c: Char, r: Rexp) : Rexp = r match {
+  case ZERO => ZERO
+  case ONE => ZERO
+  case CHAR(d) => if (c == d) ONE else ZERO
+  case ALT(r1, r2) => ALT(der(c, r1), der(c, r2))
+  case SEQ(r1, r2) =>
+    if (nullable(r1)) ALT(SEQ(der(c, r1), r2), der(c, r2))
+    else SEQ(der(c, r1), r2)
+  case STAR(r) => SEQ(der(c, r), STAR(r))
+}
 
-def replace(r: Rexp, s1: String, s2: String): String = ...
+def simp(r: Rexp): Rexp = r match {
+
+  case ALT(r1, r2) => {
+    val one = simp(r1)
+    val two = simp(r2)
+    (one, two) match {
+      case (ZERO, _) => (two)
+      case (_, ZERO) => (one)
+      case _ => if (one == two) (one)
+      else (ALT (one, two))
+    }
+  }
+  case SEQ(r1, r2) => {
+    val one = simp(r1)
+    val two = simp(r2)
+    (one, two) match {
+      case (ZERO, _) => (ZERO)
+      case (_, ZERO) => (ZERO)
+      case (ONE, _) => (two)
+      case (_, ONE) => (one)
+      case _ => (SEQ(one,two))
+    }
+  }
+
+  case r => r
+}
+
+def ders (s: List[Char], r: Rexp) : Rexp = s match {
+  case Nil => r
+  case c::s => ders(s, simp(der(c, r)))
+}
+
+def matcher(r: Rexp, s: String) : Boolean = nullable(ders(s.toList, r))
 
 
+def replace(r: Rexp, s1: String, s2: String): String =  {
+  if((nullable(r))){
+    println("Not null")
+  }else println("null")
 
-// some testing data
-// the supposedly 'evil' regular expression (a*)* b
-/*
+
+  s1
+  //case ALT(STAR(SEQ(CHAR('a'),CHAR('a'))),SEQ(CHAR('b'),CHAR('b')) )=> s1.replaceAllLiterally(s1,s2)
+  //  case ALT(STAR(SEQ(CHAR('a'),CHAR('a'))), SEQ(CHAR('b'),CHAR('b'))) =>
+  // s1.toString.replaceAll(ALT(STAR(SEQ(CHAR('a'),CHAR('a'))),SEQ(CHAR('b'),CHAR('b')) ).toString,s2)
+
+}
+
+val EVILs = ALT(STAR(SEQ(CHAR('a'),CHAR('a'))),SEQ(CHAR('b'),CHAR('b')) )
+replace(EVILs,"aabbbaaaaaaabaaaaabbaaaabb ","c")
+
 val EVIL = SEQ(STAR(STAR(CHAR('a'))), CHAR('b'))
 println(matcher(EVIL, "a" * 1000 ++ "b"))
 println(matcher(EVIL, "a" * 1000))
-
-
 def time_needed[T](i: Int, code: => T) = {
   val start = System.nanoTime()
   for (j <- 1 to i) code
@@ -99,6 +122,3 @@ def time_needed[T](i: Int, code: => T) = {
 for (i <- 1 to 5000001 by 500000) {
   println(i + " " + "%.5f".format(time_needed(2, matcher(EVIL, "a" * i))))
 }
-*/
-
-
